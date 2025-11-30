bd0f0fb1de17bab8e21c9842bf07a88d232ea7f5
xxfariddx <qarayevfarid3@gmail.com>
2025-11-30 17:08:36 +0000

    3-name

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints exactly
+ * "Programming is like building a multilingual puzzle", followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
