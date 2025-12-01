879e2d2ac8ef58ccf221d972d125028d7bcdb81c
xxfariddx <qarayevfarid3@gmail.com>
2025-12-01 10:21:57 +0000

    0-positive_or_negative.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,28 +1,17 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- * main - prints the last digit of a randomly generated number
+ * main - prints the lowercase alphabet
  *
  * Return: Always 0
  */
 int main(void)
 {
-	int n;
-	int last_digit;
+	char c;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	last_digit = n % 10;
-
-	if (last_digit > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-	else if (last_digit == 0)
-		printf("Last digit of %d is %d and is 0\n", n, last_digit);
-	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",
-		       n, last_digit);
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
 
 	return (0);
 }
