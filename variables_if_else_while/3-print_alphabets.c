3a19a26274391abb2719e0fa47b11645d04ba360
xxfariddx <qarayevfarid3@gmail.com>
2025-12-01 10:26:13 +0000

    0-positive_or_negative.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, then uppercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);   // 1st putchar
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);   // 2nd putchar
+	putchar('\n');    // 3rd putchar
+
+	return (0);
+}
