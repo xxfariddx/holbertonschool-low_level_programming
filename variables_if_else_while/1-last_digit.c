e4288063082f7db2f39003f4da1e195416947e1a
xxfariddx <qarayevfarid3@gmail.com>
2025-12-01 10:08:08 +0000

    0-positive_or_negative.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,18 +15,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-    last_digit = n % 10
+    last_digit = n % 10;
 	if ( last_digit > 5)
 	{
-		printf("%d and is greater than 5\n", n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	}
 	else if ( last_digit == 0)
 	{
-		printf("%d and is 0\n", n);
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	}
 	else
 	{
-		printf("%d and is less than 6 and not 0\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 	}
 
 	return (0);
