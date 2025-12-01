f39b7716e9f5e10ddecb2c21449b162a5167b254
xxfariddx <qarayevfarid3@gmail.com>
2025-12-01 10:53:38 +0000

    0-pos

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of single-digit numbers
+ * main - prints all possible combinations of two digits
  *
  * Return: Always 0
  */
@@ -9,20 +9,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 8; i++)
 	{
-		for (j = i + 1; j < 10; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
-			putchar(i + '0');     /* 1st putchar: print first digit */
-			putchar(j + '0');     /* 2nd putchar: print second digit */
-			if (i != 8 || j != 9)
-			{
-				putchar(',');     /* 3rd putchar: comma */
-				putchar(' ');     /* 4th putchar: space */
-			}
-		}
-	}
-	putchar('\n'); /* optional newline */
-
-	return (0);
-}
+			putchar(i + '0');  /* 1st putchar: first digit */
+			putchar(j + '0');  /* 2nd putchar: second digit */
