23ebbd59318302dd69d51846138140c5cabbee71
xxfariddx <qarayevfarid3@gmail.com>
2025-12-01 10:57:42 +0000

    0-pos

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,25 +1,21 @@
-#include <stdio.h>
-
-/**
- * main - prints all possible combinations of two digits
+#include <stdio.h>/**
+ * main - Prints all single-digit numbers of base 10
  *
  * Return: Always 0
  */
 int main(void)
 {
-	int i, j;
-
-	for (i = 0; i <= 8; i++)
-	{
-		for (j = i + 1; j <= 9; j++)
-		{
-			putchar(i + '0');           /* 1st putchar */
-			putchar(j + '0');           /* 2nd putchar */
-			if (i != 8 || j != 9)
-				putchar(44), putchar(32); /* 3rd & 4th putchar: ',' and ' ' */
-		}
-	}
-	putchar(10); /* newline using ASCII 10, allowed as outside 4-count rule */
-
-	return (0);
+    int i;    i = 0;
+    while (i <= 9)
+    {
+        putchar(i + '0');
+        if (i != 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+        i++;
+    }
+    putchar('\n');
+    return (0);
 }
