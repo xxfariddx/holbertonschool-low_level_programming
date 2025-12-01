f18513776e4fd6d8f26e071ac1d748599a7fdf3f
xxfariddx <qarayevfarid3@gmail.com>
2025-12-01 10:59:53 +0000

    0-pos

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,21 +1,28 @@
-#include <stdio.h>/**
+#include <stdio.h>
+
+/**
  * main - Prints all single-digit numbers of base 10
  *
  * Return: Always 0
  */
 int main(void)
 {
-    int i;    i = 0;
-    while (i <= 9)
-    {
-        putchar(i + '0');
-        if (i != 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-        i++;
-    }
-    putchar('\n');
-    return (0);
+	int i;
+
+	i = 0;
+
+	while (i <= 9)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
 }
