f20308ccf2c5c95cdfcfab4ff6ee614c1bb26b71
xxfariddx <qarayevfarid3@gmail.com>
2025-12-06 17:48:02 +0000

    c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,78 +1,29 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_number - prints an integer using _putchar
- * @n: number to print
+ * main - prints numbers from 1 to 100 with FizzBuzz rules
+ *
+ * Return: 0
  */
-void print_number(int n)
-{
-	int rev = 0, d, temp = n;
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
-	/* reverse the number to print digits in correct order */
-	while (temp > 0)
-	{
-		rev = rev * 10 + (temp % 10);
-		temp /= 10;
-	}
-
-	while (rev > 0)
-	{
-		d = rev % 10;
-		_putchar(d + '0');
-		rev /= 10;
-	}
-}
-
-/**
- * fizz_buzz - prints numbers from 1 to 100, replacing multiples
- * of 3 with Fizz, multiples of 5 with Buzz, and multiples of
- * both with FizzBuzz, using _putchar only.
- */
-void fizz_buzz(void)
+int main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-		{
-			_putchar('F');
-			_putchar('i');
-			_putchar('z');
-			_putchar('z');
-			_putchar('B');
-			_putchar('u');
-			_putchar('z');
-			_putchar('z');
-		}
+			printf("FizzBuzz");
 		else if (i % 3 == 0)
-		{
-			_putchar('F');
-			_putchar('i');
-			_putchar('z');
-			_putchar('z');
-		}
+			printf("Fizz");
 		else if (i % 5 == 0)
-		{
-			_putchar('B');
-			_putchar('u');
-			_putchar('z');
-			_putchar('z');
-		}
+			printf("Buzz");
 		else
-		{
-			print_number(i);
-		}
+			printf("%d", i);
 
-		if (i < 100)
-			_putchar(' ');
+		if (i != 100)
+			printf(" "); /* space after each element except the last */
 	}
+	printf("\n");
 
-	_putchar('\n');
+	return (0);
 }
