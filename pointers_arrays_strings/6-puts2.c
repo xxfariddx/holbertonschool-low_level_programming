86dc8d89f88978acbd0895637c0347e442ad85b1
xxfariddx <qarayevfarid3@gmail.com>
2025-12-09 16:08:34 +0000

    C

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,16 +6,18 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i, len = 0;
 
-	if (!str)  /* safeguard against NULL */
+	if (!str)
 		return;
 
-	while (str[i])
-	{
+	/* Find length of string */
+	while (str[len] != '\0')
+		len++;
+
+	/* Print every other character */
+	for (i = 0; i < len; i += 2)
 		_putchar(str[i]);
-		i += 2;
-	}
 
 	_putchar('\n');
 }
