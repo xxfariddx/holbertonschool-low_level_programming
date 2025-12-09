1a2409512bd7c03974e8a6477e29debe32ac8699
xxfariddx <qarayevfarid3@gmail.com>
2025-12-09 16:05:46 +0000

    C

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string, starting with the first
+ * @str: string to be printed
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2; /* move to the next character to print */
+	}
+
+	_putchar('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void rev_string(char *s);
+void puts2(char *str);
+int _putchar(char s);
 
 #endif
