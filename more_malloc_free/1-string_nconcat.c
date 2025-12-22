c4f6a863c83cd9a9c5257bffce88e131a5ae57c5
xxfariddx <qarayevfarid3@gmail.com>
2025-12-22 11:25:19 +0000

    C

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -10,36 +10,29 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *concat;
-    unsigned int len1 = 0, len2 = 0, i, j;
+	char *concat;
+	unsigned int len1 = 0, len2 = 0, i, j;
 
-    /* Treat NULL as empty string */
-    if (s1 != NULL)
-        while (s1[len1] != '\0')
-            len1++;
-    if (s2 != NULL)
-        while (s2[len2] != '\0')
-            len2++;
+	if (s1 != NULL)
+		while (s1[len1] != '\0')
+			len1++;
+	if (s2 != NULL)
+		while (s2[len2] != '\0')
+			len2++;
 
-    /* If n >= len2, use entire s2 */
-    if (n > len2)
-        n = len2;
+	if (n > len2)
+		n = len2;
 
-    /* Allocate memory for new string */
-    concat = malloc(sizeof(char) * (len1 + n + 1));
-    if (concat == NULL)
-        return (NULL);
+	concat = malloc(sizeof(char) * (len1 + n + 1));
+	if (concat == NULL)
+		return (NULL);
 
-    /* Copy s1 */
-    for (i = 0; i < len1; i++)
-        concat[i] = s1[i];
+	for (i = 0; i < len1; i++)
+		concat[i] = s1[i];
+	for (j = 0; j < n; j++)
+		concat[i + j] = s2[j];
 
-    /* Copy first n bytes of s2 */
-    for (j = 0; j < n; j++)
-        concat[i + j] = s2[j];
+	concat[i + j] = '\0';
 
-    /* Null terminate */
-    concat[i + j] = '\0';
-
-    return (concat);
+	return (concat);
 }
