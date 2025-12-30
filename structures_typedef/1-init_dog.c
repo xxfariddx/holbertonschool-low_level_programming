552fe4dbdb14cca3d4e61e1de1060e45481b13d2
xxfariddx <qarayevfarid3@gmail.com>
2025-12-30 11:24:57 +0000

    C

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,14 +1,18 @@
 #include "dog.h"
 
-
-
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to the struct dog to initialize
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    if (d == NULL)
-        return;
-    
-    d->name = xaski
-    d-> age = 5
-    d-owner = farid
-    
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
