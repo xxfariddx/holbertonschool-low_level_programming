79835b4fe7c6f090e5fc5192f6de4a08bc475c5e
xxfariddx <qarayevfarid3@gmail.com>
2025-12-30 11:23:59 +0000

    C

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+    if (d == NULL)
+        return;
+    
+    d->name = xaski
+    d-> age = 5
+    d-owner = farid
+    
+}
