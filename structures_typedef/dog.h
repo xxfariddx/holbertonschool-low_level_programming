38b8e045b34c0be42db10cfd157dc88f0821a900
xxfariddx <qarayevfarid3@gmail.com>
2025-12-30 11:48:48 +0000

    C

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,6 +15,11 @@ struct dog
 };
 
 typedef struct dog dog_t;
+
+/* Function prototypes */
 dog_t *new_dog(char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
