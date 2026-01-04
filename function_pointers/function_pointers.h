0187afac84828d5c5dc57366ab82044791245ce4
xxfariddx <qarayevfarid3@gmail.com>
2026-01-04 11:54:40 +0000

    C

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -3,7 +3,7 @@
 
 #include <stdlib.h>
 
-void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 int _putchar(char c);
 
 #endif /* FUNCTION_POINTERS_H */
