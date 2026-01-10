daba0e86a9be5f6fc90fc5f8babefc24831724f9
xxfariddx <qarayevfarid3@gmail.com>
2026-01-10 18:52:52 +0000

    args

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,56 +1,81 @@
+#include "variadic_functions.h"
 #include <stdio.h>
-#include <stdarg.h>
 
 /**
- * struct printer - Struct to map format tokens to functions
- * @symbol: The format character (c, i, f, s)
- * @print: Pointer to the function that prints that type
+ * p_char - Prints a char
+ * @arg: A list of arguments pointing to the character to be printed
  */
-typedef struct printer
+void p_char(va_list arg)
 {
-    char *symbol;
-    void (*print)(va_list arg);
-} printer_t;
-
-void p_char(va_list arg) { printf("%c", va_arg(arg, int)); }
-void p_int(va_list arg) { printf("%d", va_arg(arg, int)); }
-void p_float(va_list arg) { printf("%f", va_arg(arg, double)); }
-void p_string(va_list arg)
-{
-    char *s = va_arg(arg, char *);
-    if (!s)
-        s = "(nil)";
-    printf("%s", s);
+	printf("%c", va_arg(arg, int));
 }
 
+/**
+ * p_int - Prints an int
+ * @arg: A list of arguments pointing to the integer to be printed
+ */
+void p_int(va_list arg)
+{
+	printf("%d", va_arg(arg, int));
+}
+
+/**
+ * p_float - Prints a float
+ * @arg: A list of arguments pointing to the float to be printed
+ */
+void p_float(va_list arg)
+{
+	printf("%f", va_arg(arg, double));
+}
+
+/**
+ * p_string - Prints a string
+ * @arg: A list of arguments pointing to the string to be printed
+ */
+void p_string(va_list arg)
+{
+	char *s = va_arg(arg, char *);
+
+	if (!s)
+		s = "(nil)";
+	printf("%s", s);
+}
+
+/**
+ * print_all - Prints anything based on the format provided
+ * @format: A list of types of arguments passed to the function
+ * @...: A variable number of arguments to be printed
+ */
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    printer_t funcs[] = {
-        {"c", p_char},
-        {"i", p_int},
-        {"f", p_float},
-        {"s", p_string}
-    };
-    int i = 0, j;
-    char *sep = "";
+	va_list args;
+	int i = 0, j;
+	char *sep = "";
+	printer_t funcs[] = {
+		{"c", p_char},
+		{"i", p_int},
+		{"f", p_float},
+		{"s", p_string}
+	};
 
-    va_start(args, format);
-    while (format && format[i])
-    {
-        j = 0;
-        while (j < 4)
-        {
-            if (format[i] == *(funcs[j].symbol))
-            {
-                printf("%s", sep);
-                funcs[j].print(args);
-                sep = ", ";
-            }
-            j++;
-        }
-        i++;
-    }
-    printf("\n");
-    va_end(args);
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == *(funcs[j].symbol))
+			{
+				printf("%s", sep);
+				funcs[j].print(args);
+				sep = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+
+	printf("\n");
+	va_end(args);
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,7 +1,19 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-void print_all(const char * const format, ...);
-int _putchar(char c);
+#include <stdarg.h>
 
-#endif /* VARIADIC_FUNCTIONS_H */
+/**
+ * struct printer - Struct to map format tokens to functions
+ * @symbol: The format character (c, i, f, s)
+ * @print: Pointer to the function that prints that type
+ */
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+} printer_t;
+
+void print_all(const char * const format, ...);
+
+#endif
