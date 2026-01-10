a44d5b371bbaf0c453e1b835dd96b26cd644659e
xxfariddx <qarayevfarid3@gmail.com>
2026-01-10 15:26:01 +0000

    args

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,7 +1,7 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 int _putchar(char c);
 
 #endif /* VARIADIC_FUNCTIONS_H */
