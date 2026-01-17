248ae5135d3a4fe443d2c95162f6d68e0b460d5a
xxfariddx <qarayevfarid3@gmail.com>
2026-01-17 12:37:09 +0000

    C

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -3,16 +3,20 @@
 
 #include <stdlib.h> /* for size_t */
 
-/* Node structure */
+/**
+ * struct list_s - singly linked list node
+ * @str: string
+ * @len: length of the string
+ * @next: points to the next node
+ */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
-/* Function prototypes */
-size_t print_list(const list_t *h);
 int _putchar(char c);
+size_t print_list(const list_t *h);
 
 #endif /* LISTS_H */
