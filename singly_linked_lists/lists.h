2e5d1e028b1811c02e2aef718ac553fb1accb611
xxfariddx <qarayevfarid3@gmail.com>
2026-01-17 12:35:02 +0000

    C

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,7 +1,18 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-_putchar(char *c);
-size_t print_list(const list_t *h);
+#include <stdlib.h> /* for size_t */
 
-#endif 
+/* Node structure */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+/* Function prototypes */
+size_t print_list(const list_t *h);
+int _putchar(char c);
+
+#endif /* LISTS_H */
