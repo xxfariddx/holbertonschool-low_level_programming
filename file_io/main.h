fa4fb1ae2ee3bfa48f36317a224973931b21695b
xxfariddx <qarayevfarid3@gmail.com>
2026-01-23 21:12:41 +0000

    file_io

--- file_io/main.h
+++ file_io/main.h
@@ -1,7 +1,15 @@
-#ifndef "FILE_IO"
-#define "FILE_IO"
+#ifndef FILE_IO
+#define FILE_IO
 
+/* Standard headers for system calls and memory functions */
+#include <unistd.h>  /* read, write, close, ssize_t, STDOUT_FILENO */
+#include <fcntl.h>   /* open, file flags like O_RDONLY */
+#include <stdlib.h>  /* malloc, free */
+
+/* Function prototypes */
+
+/* Prints a single character to stdout */
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 
-#endif
+#endif /* FILE_IO */
