82430009f9fa930c280492c2dbcb7d466573afe1
xxfariddx <qarayevfarid3@gmail.com>
2026-01-25 12:54:35 +0000

    C

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,12 +1,33 @@
-#ifndef "HASH_TABLES_H"
-#define "HASH_TABLES_H"
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
 
-hash_table_t *hash_table_create(unsigned long int size);
+#include <stdlib.h>
 
+/**
+ * struct hash_node_s - Node of a hash table
+ * @key: The key, string
+ * @value: The value associated with the key
+ * @next: Pointer to the next node (collision handling)
+ */
+typedef struct hash_node_s
+{
+    char *key;
+    char *value;
+    struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ * struct hash_table_s - Hash table structure
+ * @size: Size of the array
+ * @array: Array of pointers to hash_node_t
+ */
 typedef struct hash_table_s
 {
     unsigned long int size;
-    struct hash_node_s **array; 
+    hash_node_t **array;
 } hash_table_t;
 
-#endif
+/* Function prototype */
+hash_table_t *hash_table_create(unsigned long int size);
+
+#endif /* HASH_TABLES_H */
