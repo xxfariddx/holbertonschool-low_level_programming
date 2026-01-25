788a472c8485f907d439292b13c084b6918ac7bd
xxfariddx <qarayevfarid3@gmail.com>
2026-01-25 13:03:19 +0000

    C

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -2,60 +2,35 @@
 #include <string.h>
 
 /**
- * hash_table_set - Adds an element to the hash table
- * @ht: The hash table
- * @key: The key (cannot be empty)
- * @value: The value associated with the key (will be duplicated)
+ * create_node - Creates a new hash node
+ * @key: Key string
+ * @value: Value string
  *
- * Return: 1 if success, 0 otherwise
+ * Return: Pointer to new node, or NULL if failed
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+hash_node_t *create_node(const char *key, const char *value)
 {
-	hash_node_t *new_node, *current;
-	unsigned long int index;
-	char *value_copy;
+	hash_node_t *node;
 
-	if (!ht || !key || *key == '\0' || !value)
-		return (0);
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (NULL);
 
-	value_copy = strdup(value);
-	if (!value_copy)
-		return (0);
-
-	index = key_index((const unsigned char *)key, ht->size);
-	current = ht->array[index];
-
-	/* Check if key already exists, update value */
-	while (current)
+	node->key = strdup(key);
+	if (!node->key)
 	{
-		if (strcmp(current->key, key) == 0)
-		{
-			free(current->value);
-			current->value = value_copy;
-			return (1);
-		}
-		current = current->next;
+		free(node);
+		return (NULL);
 	}
 
-	/* Key does not exist, create new node */
-	new_node = malloc(sizeof(hash_node_t));
-	if (!new_node)
+	node->value = strdup(value);
+	if (!node->value)
 	{
-		free(value_copy);
-		return (0);
+		free(node->key);
+		free(node);
+		return (NULL);
 	}
 
-	new_node->key = strdup(key);
-	if (!new_node->key)
-	{
-		free(value_copy);
-		free(new_node);
-		return (0);
-	}
-
-	new_node->value = value_copy;
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
-
-	return (1);
+	node->next = NULL;
+	return (node);
 }
